f4f474e7b0c28a705cd2a52a211b77f6b8cfcf9d
nguyentlan04@gmail.com <nguyentlan04@gmail.com>
2021-10-13 22:29:03 -0700

    Added database

---
+++ database.py
@@ -0,0 +1,17 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+from flask import *
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+data = {
+    u'name': u'Pomona',
+    u'state': u'CA',
+    u'country': u'USA'
+}
+
+db.collection('persons').document('names').set(data)
