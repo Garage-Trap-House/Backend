2591fffa84cb5cbe2821da2eb2672ebcbe40d132
nguyentlan04@gmail.com <nguyentlan04@gmail.com>
2021-10-19 02:16:10 -0700

    added two methods for http request

--- database.py
+++ database.py
@@ -3,15 +3,23 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 from flask import *
 
+# https://firebase.google.com/docs/database/admin/save-data
+# https://firebase.google.com/docs/firestore/manage-data/add-data
+
+path = "serviceAccountKey.json"
 cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 
 data = {
-    u'name': u'Los Angeles ',
-    u'state': u'CA',
-    u'country': u'USA'
+    'name': 'Pomon',
+    'state': 'CA',
+    'country': 'USA'
 }
 
-db.collection('persons').document('names').set(data)
+db.collection('map').add(data)
+
+
+house_update = db.collection('Accounts').document('Template')
+house_update.update({'house': ""})
